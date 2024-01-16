245194f553a17e35ac673fa8e9447788016e6a07
maadt <147260993+maadt@users.noreply.github.com>
2024-01-16 23:35:41 +0900

    Lesson04実装完了

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -8,7 +8,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-//Spring Securityの設定を有効にします
+//Spring Securityの設定を有効にする
 @EnableWebSecurity 
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 // WebSecurityConfigurerAdapter：セキュリティ設定用のクラスは必ず継承する
@@ -18,9 +18,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     // HttpSecurity http：HTTPリクエストに対するセキュリティ設定を行うためのクラス
     	
     	// 認可の設定
-    	http.authorizeRequests()
-            .antMatchers("/loginForm").permitAll() // /loginFormは、全ユーザからのアクセスを許可
-            .anyRequest().authenticated(); // 許可した項目以外は、認証を求める
+    	http.authorizeRequests() // HTTPリクエストに対するアクセス許可の設定を開始する
+            .antMatchers("/loginForm").permitAll() 
+            // antMatchers()：特定のURLパターンに対して適用されるアクセス許可ルールを設定
+            // "/loginForm"：/loginForm に対して
+            // permitAll()：認証なしでアクセスを許可
+            .anyRequest().authenticated();
+    	    // anyRequest()：どのリクエストに対しても適用されるアクセス許可ルールを設定
+    	    // authenticated()：認証が必要であることを示す
     	
     	//ログイン処理
     	http.formLogin()
@@ -43,40 +48,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     
     // ハッシュアルゴリズムの定義
     @Bean
+    // メソッドの戻り値をBeanとしてSpringコンテナに登録
     public PasswordEncoder passwordEncoder() {
+    // passwordEncoder()：PasswordEncoderのインスタンスを提供する
         return new BCryptPasswordEncoder();
+        // BCryptPasswordEncoder()：パスワードをハッシュ化する
     }
     
     @Override
     // WebSecurity型の引数を持ったconfigure()を追加します
     public void configure(WebSecurity web) throws Exception {
-        /** 以下のファイルパス配下のディレクトリ、ファイルすべてを認証・認可の対象から除外します
-            src/main/resources/static/css/
-            src/main/resources/static/js/
-            src/main/resources/static/images/
-        */
+    //WebSecurity web：アプリケーション内のリクエストに対するセキュリティフィルターの設定を調整する
         web.ignoring().antMatchers("/css/**", "/js/**", "/images/**");
+        // ignoring()：指定したリクエストパスをセキュリティーフィルターから除外する
     }
 }
-
-
-/*　HttpSecurity httpクラスの詳細
-URLベースのアクセス制御：
-　・antMatchers() 特定のURLパターンに対するアクセス許可や拒否のルールを設定できます。
-ログイン関連の設定：
-　・formLogin() ログインフォームやログインページのカスタマイズ、認証に使用するユーザー名とパスワードのフィールド名、
-　　　　　　　　　 ログイン成功時や失敗時のリダイレクト先などを設定できます。
-ログアウト関連の設定：
-　・logout() ログアウト時の動作を設定できます。
-認証の設定：
-　・authorizeRequests() リクエストに対するアクセス権を設定できます。
-　・permitAll() メソッドを使用して特定のリクエストをすべてのユーザーに許可したり、
-　・authenticated() メソッドを使用して認証が必要なリクエストを指定したりします。
-CSRF対策：
-　・csrf() メソッドを使用して、クロスサイトリクエストフォージェリ（CSRF）攻撃からの保護を有効または無効にできます。
-　　　　　　 CSRFトークンの生成と検証を設定できます。
-
-カスタムフィルターの追加：
- addFilterBefore()、addFilterAfter() メソッドを使用して、カスタムセキュリティフィルターを追加できます。
-                                     これにより、リクエストとレスポンスの処理をカスタマイズできます。
-*/
